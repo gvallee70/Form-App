58921469b8ec0c171d6971bd9f79fe0959ba6504
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2022-03-01 19:10:31 +0100

    Init project + embed iForm

---
+++ Form App/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Form App
+//
+//  Created by Gwendal on 01/03/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+
+
+
+}
+
---
+++ Form App/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Form App
+//
+//  Created by Gwendal on 01/03/2022.
+//
+
+import UIKit
+import iForm
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var toto = iFormTextFieldItem(placeholder: "Placeholder", text: "Texte", type: .email)
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
